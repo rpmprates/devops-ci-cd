3b6b9f69a0d96880c290aa56c30f04549aa8bfaa
Raquel Prates <rpmprates@gmail.com>
2026-03-16 19:07:23 -0300

    fix: start spring boot before frontend tests

--- frontend/test/frontend.test.js
+++ frontend/test/frontend.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('deve carregar tela de consulta', async ({ page }) => {
 
-  await page.goto('/index.html');
+  await page.goto('http://localhost:8080/index.html');
 
   await expect(page.locator('h2')).toHaveText('Consultar Saldo');
 
